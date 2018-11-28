8b9e272c254aa0ddc58fa9a39cf306f91dc218eb
DUCXDO <nguyenminhduc9197@gmail.com>
2018-11-28 23:34:54 +0700

    Ver 1

---
+++ Assets/Script/InteracScript.cs
@@ -0,0 +1,75 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class InteracScript : MonoBehaviour
+{
+    public Camera camera;
+    public float range = 10f;
+    public Text interactText;
+    public KeyCode InteractKey = KeyCode.F;
+    // Update is called once per frame
+    void Update()
+    {
+
+        ShowTarget();
+
+    }
+
+    void ShowTarget()
+    {
+        RaycastHit hit;
+        if (Physics.Raycast(camera.transform.position, camera.transform.forward, out hit, range))
+        {
+            InteractableObject obj = hit.transform.GetComponent<InteractableObject>();
+            
+            if (obj != null)
+            {
+                OpenDoorScript ods = hit.transform.GetComponent<OpenDoorScript>();
+                if (ods != null)
+                {
+                    if (ods.DoorState == false)
+                    {
+                        interactText.text = "Press 'F' to open door";
+                        interactText.enabled = true;
+                    }
+                    else
+                    {
+                        interactText.text = "Press 'F' to close door";
+                        interactText.enabled = true;
+                    }
+                }
+                else
+                {
+                    interactText.text = "Press 'F' to interact";
+                    interactText.enabled = true;
+                }
+            }
+            if (Input.GetKeyDown(InteractKey) == true)
+            {
+                Interact();
+            }
+        }
+        else
+        {
+            interactText.enabled = false;
+        }
+    }
+
+    void Interact()
+    {
+        RaycastHit hit;
+        if (Physics.Raycast(camera.transform.position, camera.transform.forward, out hit, range))
+        {
+            InteractableObject obj = hit.transform.GetComponent<InteractableObject>();
+            if (obj != null)
+            {
+                OpenDoorScript ods = hit.transform.GetComponent<OpenDoorScript>();
+                if (ods != null)
+                {
+                    ods.OpenDoor();
+                }
+
+            }
+        }
+    }
+}
---
+++ Assets/Script/OpenDoorScript.cs
@@ -0,0 +1,26 @@
+﻿
+using UnityEngine;
+
+public class OpenDoorScript : MonoBehaviour {
+    public GameObject Door;
+    public bool DoorState = false;
+
+    public void OpenDoor()
+    {
+        Animator ani = Door.GetComponent<Animator>();
+        if (ani != null)
+        {
+            ani.SetTrigger("OpenDoor");
+            if(DoorState == false)
+            {
+                DoorState = true;
+            }
+            else
+            {
+                DoorState = false;
+            }
+        }
+    }
+
+
+}
